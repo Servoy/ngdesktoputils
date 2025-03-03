4557aae1bfa535154411042888048a0e515637c0
mvid <mvid@servoy.com>
2025-03-03 11:17:47 +0200

    Revert "SVY-19830 Review and update all _doc.js files from documented plugins"
    This reverts commit 968287721a24a691d443170b94f8cbe91e97f4b2.

--- ngdesktoputils/ngdesktoputils/ngdesktoputils_doc.js
+++ ngdesktoputils/ngdesktoputils/ngdesktoputils_doc.js
@@ -6,9 +6,6 @@ function exit() {
 }
 /**
  * Executes a command async, the server side call will not block on this call.
- * 
- * @param {String} program The executable or command to run.
- * @param {Array<Object>} args An array of arguments to pass to the command.
  */
 function executeCommand(program,args) {
 }
@@ -16,10 +13,6 @@ function executeCommand(program,args) {
  * This executes a command and returns the result of the stdout 
  * This will also call reject when a error happens so the call will error out. (will not return correct)
  * Try to use the async executeCommand so nothing will be blocking.
- * 
- * @param {String} program The executable or command to run.
- * @param {Array<Object>} args An array of arguments to pass to the command.
- * @return {String} The standard output result of the executed command.
  */
 function executeCommandSync(program,args) {
 }
@@ -35,8 +28,6 @@ function setClipboardContent(text)
 
 /**
  * Get clipboard text. 
- * 
- * @return {String} The current text content stored in the clipboard.
  */
 function getClipboardContent()
 {
@@ -44,8 +35,6 @@ function getClipboardContent()
 
 /**
  * Whether application is running as NGDesktop. 
- * 
- * @return {Boolean} True if the application is running in NGDesktop mode; otherwise, false.
  */
 function isNGDesktop()
 {
@@ -53,9 +42,6 @@ function isNGDesktop()
 
  /**
  * Prints a pdf document specified by path. Optionally, specify printer or unix print options (lp command) or windows print options see: https://github.com/Servoy/pdf-to-printer
- * 
- * @param {String} path The file path of the PDF document to be printed.
- * @param {CustomType<ngdesktoputils.printoptions>} [options] Optional print settings, including printer selection and specific print parameters for Unix (lp command) or Windows.
  */
 function printPDF (path, options) 
 {
@@ -63,16 +49,12 @@ function printPDF (path, options)
 
 /**
  * Returns installed printers on local machine.
- * 
- * @return {Array<CustomType<ngdesktoputils.printer>>} An array of printer objects representing installed printers.
  */
 function getPrinters () 
 {
 }
 /**
  * Returns default printer on local machine.
- * 
- * @return {CustomType<ngdesktoputils.printer>} The default printer object.
  */
 function getDefaultPrinter () 
 {
@@ -80,7 +62,7 @@ function getDefaultPrinter ()
 /**
  * Manage URLs using their default applications
  * 
- * @param {String} url The URL to be opened in the default web browser or application.
+ * @param {String} url 
  */
 function showExternal (url)
 {
@@ -89,7 +71,7 @@ function showExternal (url)
 /**
  * Retrieve information from the ngdesktop client
  * 
- * @return {CustomType<ngdesktoputils.sysinfo>} An object containing the client's system information:
+ * @return an object containing the client's system information:
  * 
  *     ngDesktopVersion    - string: ngdesktop version
  *     osPlatform          - string: can be 'darwin', 'linux', 'freebsd', 'openbsd' and 'win32'
