bac0acc3fa7e320d677cae53ec5756c65e70acba
mvid <mvid@servoy.com>
2025-03-31 17:59:28 +0300

    SVY-19926 SpecMarkdownGenerator - incomplete/missing documentation for handlers

--- ngdesktoputils/ngdesktoputils/ngdesktoputils_doc.js
+++ ngdesktoputils/ngdesktoputils/ngdesktoputils_doc.js
@@ -13,6 +13,11 @@ function executeCommand(program,args) {
  * This executes a command and returns the result of the stdout 
  * This will also call reject when a error happens so the call will error out. (will not return correct)
  * Try to use the async executeCommand so nothing will be blocking.
+ * 
+ * @param {String} program The command to be executed
+ * @param {Array<Object>} args The command arguments
+ * 
+ * @return {String} The result from standard output.
  */
 function executeCommandSync(program,args) {
 }
@@ -28,6 +33,8 @@ function setClipboardContent(text)
 
 /**
  * Get clipboard text. 
+ * 
+ * @return {String} The clipboard text.
  */
 function getClipboardContent()
 {
@@ -35,6 +42,8 @@ function getClipboardContent()
 
 /**
  * Whether application is running as NGDesktop. 
+ * 
+ * @return {Boolean} True if app running inside NGDesktop, false otherwise.
  */
 function isNGDesktop()
 {
@@ -49,29 +58,33 @@ function printPDF (path, options)
 
 /**
  * Returns installed printers on local machine.
+ * 
+ * @return {Array<CustomType<ngdesktoputils.printer>>} An array of printer objects.
  */
-function getPrinters () 
+function getPrinters()
 {
 }
 /**
  * Returns default printer on local machine.
+ * 
+ * @return {CustomType<ngdesktoputils.printer>} The default printer object.
  */
-function getDefaultPrinter () 
+function getDefaultPrinter() 
 {
 }
 /**
  * Manage URLs using their default applications
  * 
- * @param {String} url 
+ * @param {String} url The URL to be opened.
  */
-function showExternal (url)
+function showExternal(url)
 {
 }
 
 /**
  * Retrieve information from the ngdesktop client
  * 
- * @return an object containing the client's system information:
+ * @return {CustomType<ngdesktoputils.sysinfo>} an object containing the client's system information:
  * 
  *     ngDesktopVersion    - string: ngdesktop version
  *     osPlatform          - string: can be 'darwin', 'linux', 'freebsd', 'openbsd' and 'win32'
@@ -86,39 +99,69 @@ function getSystemInformation()
 
 var svy_types = {
 
-    sysinfo: {
-
-        electronVersion : null,
-
-        ngDesktopVersion : null,
-
-        osPlatform : null,
-
-        osRelease : null,
-
-        osTotalMem : null,
-
-        osFreeMem : null,
-
-    },
-
-    printer: {
-
-        deviceId : null,
-
-        name : null,
-
-        paperSizes : null,
-
-    },
-
-    printoptions: {
-
-        printer : null,
-
-        unix : null,
-
-        win32 : null,
-
-    }
+    /**
+   * Contains system information from the NGDesktop client.
+   */
+  sysinfo: {
+    /**
+     * The version of Electron used by NGDesktop.
+     */
+    electronVersion: null,
+    /**
+     * The NGDesktop version.
+     */
+    ngDesktopVersion: null,
+    /**
+     * The operating system platform (e.g. 'darwin', 'win32', etc.).
+     */
+    osPlatform: null,
+    /**
+     * The operating system release version.
+     */
+    osRelease: null,
+    /**
+     * The total system memory in bytes.
+     */
+    osTotalMem: null,
+    /**
+     * The available (free) system memory in bytes.
+     */
+    osFreeMem: null,
+  },
+  
+  /**
+   * Contains printer information.
+   */
+  printer: {
+    /**
+     * The device identifier for the printer.
+     */
+    deviceId: null,
+    /**
+     * The name of the printer.
+     */
+    name: null,
+    /**
+     * An array of supported paper sizes.
+     */
+    paperSizes: null,
+  },
+  
+  /**
+   * Contains print options for different platforms.
+   */
+  printoptions: {
+    /**
+     * The printer identifier or name to use.
+     */
+    printer: null,
+    /**
+     * An array of Unix-specific printing options.
+     */
+    unix: null,
+    /**
+     * An array of Windows-specific printing options.
+     */
+    win32: null,
+  }
 }
